d3c30354f6f9fc3138715d2dba6218ef4bc10e1f
Nutti Saelor <leelorz6@gmail.com>
2022-04-24 13:37:47 +0700

    modify contract

--- assembly/model.ts
+++ assembly/model.ts
@@ -1,15 +1,18 @@
-import { context, u128, PersistentVector } from "near-sdk-as";
+import { context, PersistentVector, u128 } from "near-sdk-as";
 
-/** 
+/**
  * Exporting a new class PostedMessage so it can be used outside of this file.
  */
 @nearBindgen
 export class PostedMessage {
   premium: boolean;
   sender: string;
+  createdAt: u64;
   constructor(public text: string) {
-    this.premium = context.attachedDeposit >= u128.from('10000000000000000000000');
+    this.premium =
+      context.attachedDeposit >= u128.from("10000000000000000000000");
     this.sender = context.sender;
+    this.createdAt = context.blockTimestamp;
   }
 }
 /**
@@ -18,4 +21,4 @@ export class PostedMessage {
  * The parameter to the constructor needs to be unique across a single contract.
  * It will be used as a prefix to all keys required to store data in the storage.
  */
-export const messages = new PersistentVector<PostedMessage>("m");
+export const messages = new PersistentVector<PostedMessage>("gb");
