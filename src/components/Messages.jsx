8ef4cf2d2a7703e1aa541d84ed40a98a3f5bdf80
Nutti Saelor <leelorz6@gmail.com>
2022-04-24 14:40:12 +0700

    modify frontend

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const NANO_SECOND_TIMESTAMP = 1 * 10 ** 6
 
 export default function Messages({ messages }) {
   return (
     <>
       <h2>Messages</h2>
-      {messages.map((message, i) =>
-        // TODO: format as cards, add timestamp
-        <p key={i} className={message.premium ? 'is-premium' : ''}>
-          <strong>{message.sender}</strong>:<br/>
-          {message.text}
-        </p>
-      )}
+      {messages
+        .sort((a, b) => b.createdAt - a.createdAt)
+        .map((message, i) => (
+          <p key={i} className={message.premium ? 'is-premium' : ''}>
+            <strong>{message.sender}</strong>:<br />
+            <div>{message.text}</div>
+            <div>
+              {new Date(
+                message.createdAt / NANO_SECOND_TIMESTAMP,
+              ).toLocaleString()}
+            </div>
+          </p>
+        ))}
     </>
-  );
+  )
 }
 
 Messages.propTypes = {
-  messages: PropTypes.array
-};
+  messages: PropTypes.array,
+}
